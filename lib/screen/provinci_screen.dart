5aa6aec7560c1e2c00ef6128b7f6f0c33f5efa84
TahsinulAmir <tahsinulamir1@gmail.com>
2023-01-28 11:45:45 +0700

    COVID APP I: Fetching data berhasil dengan statuscode 200

--- lib/screen/provinci_screen.dart
+++ lib/screen/provinci_screen.dart
@@ -1,16 +1,24 @@
+import 'package:covid_project/providers/data.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ProvinsiScreen extends StatelessWidget {
   const ProvinsiScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Data dataCovid = Provider.of<Data>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Covid App"),
       ),
       body: Center(
-        child: ElevatedButton(onPressed: () {}, child: Text("Fetch")),
+        child: ElevatedButton(
+          onPressed: () {
+            dataCovid.fetchData();
+          },
+          child: Text("Fetch"),
+        ),
       ),
     );
   }
