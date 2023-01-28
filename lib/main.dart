d2bd43a2534b3a14e89f19ef851a0e96cf586307
TahsinulAmir <tahsinulamir1@gmail.com>
2023-01-28 11:35:36 +0700

    COVID APP I: Fetching data & create provider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:covid_project/providers/data.dart';
 import 'package:covid_project/screen/provinci_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -12,7 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ProvinsiScreen(),
+      home: ChangeNotifierProvider(
+        create: (context) => Data(),
+        child: ProvinsiScreen(),
+      ),
     );
   }
 }
--- lib/providers/data.dart
+++ lib/providers/data.dart
@@ -1,6 +1,13 @@
 import 'package:covid_project/models/data_covid.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class Data with ChangeNotifier {
   List<DataCovid> dataProvinsi = [];
+
+  Future<void> fetchData() async {
+    String url = "https://data.covid19.go.id/public/api/prov.json";
+    var response = await http.get(Uri.parse(url));
+    print(response.statusCode);
+  }
 }
