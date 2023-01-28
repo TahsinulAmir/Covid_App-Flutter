882259c519271084c12b3b41f6b03467a97fdfe0
TahsinulAmir <tahsinulamir1@gmail.com>
2023-01-28 12:14:27 +0700

    COVID APP I: Menampilkan data di provinci_screen.dart dengan widget FutureBuilder

--- lib/screen/provinci_screen.dart
+++ lib/screen/provinci_screen.dart
@@ -9,17 +9,29 @@ class ProvinsiScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Data dataCovid = Provider.of<Data>(context);
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Covid App"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            dataCovid.fetchData();
-          },
-          child: Text("Fetch"),
+        appBar: AppBar(
+          title: Text("Covid App"),
         ),
-      ),
-    );
+        body: FutureBuilder(
+          future: dataCovid.fetchData(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              return ListView.builder(
+                itemCount: dataCovid.dataProvinsi.length,
+                itemBuilder: (context, index) {
+                  final data = dataCovid.dataProvinsi[index];
+                  return ListTile(
+                    title: Text(data.key!),
+                  );
+                },
+              );
+            }
+            ;
+          },
+        ));
   }
 }
