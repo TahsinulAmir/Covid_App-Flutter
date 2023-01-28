892310f84c9d9faaddc242e0ef2d9a7a25d81be9
TahsinulAmir <tahsinulamir1@gmail.com>
2023-01-28 13:17:02 +0700

    COVID APP III: Wrap to GestureDetector & create detail screen

---
+++ lib/screen/detail_provinsi.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DetailProvinsi extends StatelessWidget {
+  const DetailProvinsi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detail"),
+      ),
+    );
+  }
+}
--- lib/widget/provinsi_item.dart
+++ lib/widget/provinsi_item.dart
@@ -1,4 +1,5 @@
 import 'package:covid_project/models/data_covid.dart';
+import 'package:covid_project/screen/detail_provinsi.dart';
 import 'package:flutter/material.dart';
 
 class ProvinsiItem extends StatelessWidget {
@@ -7,31 +8,40 @@ class ProvinsiItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        border: Border.all(color: Colors.black),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            dataProvinsi.key!,
-            style: TextStyle(fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 5,
-          ),
-          Text("Jumlah Kasus"),
-          SizedBox(
-            height: 5,
-          ),
-          Text(
-            dataProvinsi.jumlahKasus.toString(),
-            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red),
-          ),
-        ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => DetailProvinsi(),
+            ));
+      },
+      child: Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: Colors.black),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              dataProvinsi.key!,
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text("Jumlah Kasus"),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              dataProvinsi.jumlahKasus.toString(),
+              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.red),
+            ),
+          ],
+        ),
       ),
     );
   }
